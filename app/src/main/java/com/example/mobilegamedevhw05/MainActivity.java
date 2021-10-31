1025988c1a05b9f394582333c3ed6be01721f7fa
Metehan <metehan@Metehan-MacBook-Pro.local>
2021-10-30 22:05:37 -0400

    Added mapFragment and button

--- app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
+++ app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 
 public class MainActivity extends AppCompatActivity {
 
     GoogleMap map;
+    Button btnGetDirection;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btnGetDirection = findViewById(R.id.btnGetDirection);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag);
 
 
     }
