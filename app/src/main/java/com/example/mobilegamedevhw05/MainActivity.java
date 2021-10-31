32b44d29f84071495e6ab34d6743dd752a9d4093
Metehan <metehan@Metehan-MacBook-Pro.local>
2021-10-30 22:49:31 -0400

    Added place coordinates

--- app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
+++ app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
@@ -8,13 +8,18 @@ import android.widget.Button;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 
 
 public class MainActivity extends AppCompatActivity {
 
     GoogleMap map;
     Button btnGetDirection;
-
+    MarkerOptions place1 , place2;
+    Polyline currentPolyLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnGetDirection = findViewById(R.id.btnGetDirection);
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+        place1 = new MarkerOptions().position(new LatLng(42.08995811748055, -75.97092185437832).title("Location 1"));
+        place1 = new MarkerOptions().position(new LatLng(41.10710624802906, 29.016927094672752).title("Location 1"));
+
 
 
     }
+    @Override
+    public void onMapReady (GoogleMap googleMap){
+        map = googleMap;
+
+    }
+
+
+
 }
