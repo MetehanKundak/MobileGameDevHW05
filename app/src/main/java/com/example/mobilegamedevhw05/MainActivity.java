2e7ede7543147b5a972477880f2dadad1320648f
Metehan <metehan@Metehan-MacBook-Pro.local>
2021-10-31 17:05:55 -0400

    Added fetch url

--- app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
+++ app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
@@ -12,9 +12,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.thecodecity.mapsdirection.R;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap map;
     Button btnGetDirection;
@@ -27,20 +28,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnGetDirection = findViewById(R.id.btnGetDirection);
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag);
-        mapFragment.getMapAsync((OnMapReadyCallback) this);
+        mapFragment.getMapAsync(this);
 
         place1 = new MarkerOptions().position(new LatLng(42.08995811748055, -75.97092185437832).title("Location 1"));
-        place1 = new MarkerOptions().position(new LatLng(41.10710624802906, 29.016927094672752).title("Location 1"));
+        place2 = new MarkerOptions().position(new LatLng(41.10710624802906, 29.016927094672752).title("Location 1"));
 
-
+        String url = getUrl (place1.getPosition(),place2.getPosition(),"driving");
+        new FetchURL(MapActivity.this).execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving");
 
     }
+
+
     @Override
-    public void onMapReady (GoogleMap googleMap){
+    public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
     }
+    private String getUrl (LatLng origin, LatLng dest, String directionMode){
+
+        String str_origin = "origin" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination" + dest.latitude + ","+ dest.longitude;
+        String mode = "mode" + directionMode;
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters+"&key="+getString(R.string.google_maps_key);
+        return url;
+    }
 
 
-
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null)
+            currentPolyline.remove();
+        currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
+    }
 }
