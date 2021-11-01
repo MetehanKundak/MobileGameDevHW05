83a8aef5ec3d4ddb178df1efc03ba1c5fbb7284b
Metehan <metehan@Metehan-MacBook-Pro.local>
2021-10-31 22:38:42 -0400

    minor changes applied

--- app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
+++ app/src/main/java/com/example/mobilegamedevhw05/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Fragment fragment = new MapFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
+        Fragment frag = new MapFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,frag).commit();
 
     }
 }
